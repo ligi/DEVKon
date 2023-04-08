1fcc745094adeed0d46e18c9fdb2c4ce737125d9
ligi <ligi@ligi.de>
2023-04-08 17:03:04 +0200

    Prevent transcript files from being regenerated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ fun main() {
 
     File("../api/data/sessions/").listFiles()?.forEach { edition ->
         println("Entering $edition")
-        edition.listFiles { _, fileName -> fileName.endsWith(".json") }?.forEach { session ->
+        edition.listFiles { _, fileName -> fileName.endsWith(".json") && !fileName.endsWith(".transcript.json") }?.forEach { session ->
             println("processing $session")
             val obj = Parser.default().parse(session.inputStream()) as JsonObject
             val mp4file = File(edition, session.nameWithoutExtension + ".mp4")
@@ -30,10 +30,13 @@ fun main() {
 
                 val wavFile = File(edition, mp4file.nameWithoutExtension + ".wav")
                 if (!wavFile.exists()) {
+                    println("Generating wav file for whisper at ${wavFile.absoluteFile}")
                     execute(edition, "ffmpeg -i ${mp4file.name} -ar 16000 -ac 1 -c:a pcm_s16le $wavFile")
                 }
                 val transcriptFile = File(edition, mp4file.nameWithoutExtension + ".transcript")
-                if (!transcriptFile.exists()) {
+                val transcriptFileWithExtension = File(edition, mp4file.nameWithoutExtension + ".transcript.json") // whisper will be adding "json" as of -oj
+                if (!transcriptFileWithExtension.exists()) {
+                    println("Transcribing via whisper")
                     execute(File("/home/devops/whisper.cpp"),"./main -f ${wavFile.absoluteFile} -of ${transcriptFile.absoluteFile} -oj")
                 }
             } else {
